5c5885d07789c053ab41363a3ac4edeb7ae9a885
elier lopez <reile18@msn.com>
2018-09-04 14:23:55 -0500

    as

--- src/AllUsersComponent.js
+++ src/AllUsersComponent.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
-
+const users = [
+  {
+    FirstName: 'Elier',
+    LastName: 'Lopez'
+  },
+  {
+    FirstName: 'Adriana',
+    LastName: 'Elizabeth'
+  },  
+];
+localStorage.setItem('products',JSON.stringify(users));
 class AllUsersComponent extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      users:[]
+    };
+  }
+  componentWillMount(){
+    const users = JSON.parse(localStorage.getItem('users'))
+  }
   render() {
     return (
-           <h1 className="App-title">The user list</h1>
+          <div>
+          <h1 className="App-title">The user list</h1>
+          <div>
+            {
+              this.state.users.map(user=> {
+              return (
+                <diV key={user.name}>
+                  <span>{user.name}</span>
+                </diV>
+              );
+              })
+            }
+          </div>
+        </div>
       );
   }
 }
